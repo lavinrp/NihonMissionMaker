9d95ad11b6f99faefe6c0272804bbec41139a439
Ryan Lavin <lavinrp@mail.uc.edu>
2015-09-25 21:31:55 -0400

    Added ability to read map names and their extensions from the Maps.txt file

--- MissionViewPage.xaml.cs
+++ MissionViewPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace Nihon_Mission_Maker
 {
@@ -44,12 +45,23 @@ namespace Nihon_Mission_Maker
         //bool to prevent save button from being enabled from components loading
         bool trackChanges = false;
 
+        //Stores the known maps and their extensions
+        ArmaMaps mapsToDisplay;
+
+        /// <summary>
+        /// Basic setup for the mission view page
+        /// </summary>
+        /// <param name="bwmfFilePath"></param>
         public MissionViewPage(string bwmfFilePath)
         {
             //store the file path for the BWMF
             this.bwmfFilePath = bwmfFilePath;
 
             InitializeComponent();
+
+            //initialize map storage
+            mapsToDisplay = new ArmaMaps();
+            LoadKnowMapsFromFile();
 
             LoadMissionElementsFromDirectory();
 
@@ -119,9 +131,14 @@ namespace Nihon_Mission_Maker
             //set mission file name to substring
             missionFileName.Text = missionFileNameString;
 
+            //set the combo box to the index of the given extension
+            mapNameComboBox.SelectedIndex = mapsToDisplay.extensions.IndexOf(extension);
+
+            //TODO: read the map name and extension from
             //set map based on extension or to BWMF if its empty
-            switch (extension)
+            /*switch (extension)
             {
+                
                 case "anim_helvantis_v2":
                     mapNameComboBox.SelectedItem = HelvantisMap;
                     break;
@@ -134,7 +151,58 @@ namespace Nihon_Mission_Maker
                 default:
                     mapNameComboBox.SelectedItem = bwmfTemplate;
                     break;
+            }*/
+        }
+
+        /// <summary>
+        /// Reads in maps found from the formated maps.txt file, associates the map names with their extensions through the
+        /// ArmaMaps Class and add the map names to the mapNamesComboBox
+        /// </summary>
+        private void LoadKnowMapsFromFile()
+        {
+            //load in the maps file as a string. Trow error if this cannot be completed
+            try
+            {
+                //stores the path to the text file containing the maps
+                //string mapFileLocation = @"C:\Users\R Lavin\Desktop\Programing\C#\Nihon Mission Maker\Nihon Mission Maker\Maps.txt";
+                string mapFileLocation = @".\Maps.txt";
+                if (File.Exists(mapFileLocation))
+                {
+                    using (StreamReader sr = new StreamReader(mapFileLocation))
+                    {
+                        //string to hold unaltered line containing the formated map display aneme
+                        string rawMapAndExtension;
+
+                        //read in maps and add them to the GUI until the end of the stream is reached
+                        while (!sr.EndOfStream)
+                        {
+                            //read in the line
+                            rawMapAndExtension = sr.ReadLine();
+
+                            //Whitespace should not be present. Break if white space is found
+                            if (String.IsNullOrWhiteSpace(rawMapAndExtension))
+                            {
+                                break;
+                            }
+
+                            //store the map
+                            mapsToDisplay.AddMap(rawMapAndExtension);
+                        }
+                    }
+                }
+                else
+                {
+                    throw (new System.IO.IOException("File Not Found"));
+                }
             }
+
+            catch
+            {
+                MessageBox.Show("Error reading map file", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+
+            //set mapNameComboBox items to the maps found
+            mapNameComboBox.ItemsSource = mapsToDisplay.mapNames;
         }
 
         //TODO: make this function follow some kind of logic (or at least be less shit)
@@ -159,7 +227,7 @@ namespace Nihon_Mission_Maker
             //if no slash found return an empty string
             throw new System.IO.FileNotFoundException();
             //return "";
-  
+
         }
 
         /// <summary>
@@ -174,7 +242,7 @@ namespace Nihon_Mission_Maker
             {
                 if (File.Exists(descriptionExtPath))
                 {
-                    using(StreamReader sr = new StreamReader(descriptionExtPath))
+                    using (StreamReader sr = new StreamReader(descriptionExtPath))
                     {
                         descriptionExtTxt = sr.ReadToEnd();
                     }
@@ -194,7 +262,7 @@ namespace Nihon_Mission_Maker
                 MessageBox.Show("Error reading description.ext file", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
             }
 
-            
+
 
         }
 
@@ -265,25 +333,7 @@ namespace Nihon_Mission_Maker
         private void RenameMissionDirectory()
         {
             //Determine what file extension to add based on the selected map
-            string fileType;
-            switch (((ComboBoxItem)mapNameComboBox.SelectedItem).Content.ToString())
-            {
-                case "Helvantis":
-                    fileType = ".anim_helvantis_v2";
-                    break;
-                case "Altis":
-                    fileType = ".Altis";
-                    break;
-                case "Stratis":
-                    fileType = ".Stratis";
-                    break;
-                case "BWMF Template":
-                    fileType = "";
-                    break;
-                default:
-                    fileType = "";
-                    break;
-            }
+            string fileType = "." + mapsToDisplay.extensions[mapNameComboBox.SelectedIndex];//mapsToDisplay.NameToExtension(((ComboBoxItem)mapNameComboBox.SelectedItem).Content.ToString());
 
             //validate directory name
             if (missionFileName.Text.Contains("<") || missionFileName.Text.Contains(">") || missionFileName.Text.Contains(":") || missionFileName.Text.Contains("\"") || missionFileName.Text.Contains("/")
@@ -331,7 +381,7 @@ namespace Nihon_Mission_Maker
                 //change the color of the element to show it can be saved
                 SaveButton.Background = enabledColor;
             }
-            
+
         }
 
         /// <summary>
@@ -398,7 +448,7 @@ namespace Nihon_Mission_Maker
             foreach (Match nameMatch in matches)
             {
                 authorName.Text = nameMatch.ToString();
-            }     
+            }
         }
 
         /// <summary>
@@ -408,7 +458,7 @@ namespace Nihon_Mission_Maker
         private void DisplayMission(ref string description)
         {
             var matches = Regex.Matches(description, missionNameRegexExpression, RegexOptions.Singleline);
-            foreach(Match missionMatch in matches)
+            foreach (Match missionMatch in matches)
             {
                 missionDisplayName.Text = missionMatch.ToString();
             }
@@ -418,7 +468,7 @@ namespace Nihon_Mission_Maker
         /// Uses regex to determine if the mission is a coop or tvt using information from a given description.ext
         /// </summary>
         /// <param name="description">text as a string from a BWMF description.ext</param>
-        private void  DisplayMissionType(ref string description)
+        private void DisplayMissionType(ref string description)
         {
             var matches = Regex.Matches(description, missionTypeRegexExpression, RegexOptions.Singleline);
             foreach (Match missionTypeMatch in matches)
@@ -438,6 +488,96 @@ namespace Nihon_Mission_Maker
 
 
         #endregion
+
+    }
+
+    /// <summary>
+    /// Class that associates map names with their Arma map extensions
+    /// </summary>
+    public class ArmaMaps
+    {
+        /// <summary>
+        /// Collection of map names to be displayed.
+        /// used with combo box
+        /// Index corresponds directly to those of extensions
+        /// </summary>
+        public ObservableCollection<string> mapNames;
+
+        /// <summary>
+        /// List containing the extensions for the maps in mapNames
+        /// Index corresponds directly to those of mapNames
+        /// </summary>
+        public List<string> extensions;
+
+        /// <summary>
+        /// Constructor for ArmaMap. Takes a string and splits it across a tab to get the display name and the extension
+        /// </summary>
+        /// <param name="mapLine">Single line string with the map display name and map extension separated by a tab</param>
+        public ArmaMaps()
+        {
+            //collection to display with the map name combo box
+            mapNames = new ObservableCollection<string>();
+            extensions = new List<string>();
+            mapNames.Add("BWMF template");
+            extensions.Add("");
+        }
+
+        /// <summary>
+        /// Splits properly formated string into two parts and stores them into the mapNames and extensions
+        /// </summary>
+        /// <param name="mapLine">formated string with map display name and map extension separated by a single tab</param>
+        public void AddMap(string mapLine)
+        {
+            //split input
+            string[] mapAndextensionArray = mapLine.Split('\t');
+            //add map and extension to correct array
+            mapNames.Add(mapAndextensionArray[0]);
+            extensions.Add(mapAndextensionArray[1]);
+        }
+
+        /// <summary>
+        /// returns the extension text associated with the given map name
+        /// if the map name is not found returns the default extension
+        /// </summary>
+        /// <param name="mapName">map name associated with desired extension</param>
+        /// <returns>extension associated with given map name</returns>
+        public string NameToExtension(string mapName)
+        {
+            int index = mapNames.IndexOf(mapName);
+
+            //if the index is valid return the associated extension
+            if (index >= 0 && index < mapNames.Count)
+            {
+                return extensions[index];
+            }
+            //if the index is not valid return the default
+            else
+            {
+                return extensions[0];
+            }
+        }
+
+        /// <summary>
+        /// returns the map name text associated with the given extension
+        /// </summary>
+        /// <param name="extension">extension associated with desired map name</param>
+        /// <returns>map name associated with given extension</returns>
+        public string ExtensionToName(string extension)
+        {
+            int index = extensions.IndexOf(extension);
+
+            //if the index is valid return the associated map name
+            if (index >= 0 && index < extensions.Count)
+            {
+                return mapNames[index];
+            }
+
+            //if the index is not valid return the default
+            else
+            {
+                return mapNames[0];
+            }
+        }
 
     }
 }
