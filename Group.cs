a71801d3abb87212401c70a72eb4364d8f5409dc
Ryan Lavin <lavinrp@mail.uc.edu>
2015-12-26 23:23:39 -0500

    Added Group and Unit from no GUI version of NMM

---
+++ Group.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupMakerNoGui
+{
+    class Group
+    {
+        public Group()
+        {
+            //assign default values to the group
+            side = "WEST";
+            tag = "FAIL";
+            unitCount = 0;
+
+            units = new List<Unit>();
+        }
+
+        public Group(string side, int unitCount, string tag, List<Unit> units)
+        {
+            //assign specific values to the group
+            this.side = side;
+            this.UnitCount = unitCount;
+            this.tag = tag;
+            this.units = units;
+        }
+
+
+        /// <summary>
+        /// Creates a group with a given side, unitCount and tag but with default units for its faction
+        /// </summary>
+        /// <param name="side">Faction the group belongs to</param>
+        /// <param name="unitCount">Number of units in the group</param>
+        /// <param name="tag">Name on the groups map marker</param>
+        /// <returns>Group with default units</returns>
+        public Group(string side, int unitCount, string tag)
+        {
+
+            //create a list to store units
+            List<Unit> units = new List<Unit>();
+
+            //fill unit list with units
+            for (int i = 0; i < unitCount; ++i)
+            {
+                Unit tempUnit = new Unit(side);
+                units.Add(tempUnit);
+            }
+
+
+            //assign specific values to the group
+            this.side = side;
+            this.UnitCount = unitCount;
+            this.tag = tag;
+        }
+
+
+        //Getters and setters for all member variables
+
+        //getter and ssetter for side
+        public string Side
+        {
+            get
+            {
+                return side;
+            }
+
+            set
+            {
+                side = value;
+            }
+        
+        }
+        //getter and setter for unitCount
+        public int UnitCount
+        {
+            get
+            {
+                return unitCount;
+            }
+            set
+            {
+                unitCount = value;
+            }
+        }
+        //getter and setter for tag
+        public string Tag
+        {
+            get
+            {
+                return tag;
+            }
+
+            set
+            {
+                tag = value;
+            }
+        }
+
+        /// <summary>
+        /// adds a unit to the group
+        /// </summary>
+        /// <param name="unit"> the unit to add </param>
+        public void AddUnit(Unit unit)
+        {
+            units.Add(unit);
+        }
+
+        /// <summary>
+        /// removes the unit at the specified location from the list of units
+        /// </summary>
+        /// <param name="location"> the location of the unit to remove </param>
+        public void RemoveUnit(int location)
+        {
+            units.RemoveAt(location);
+        }
+
+        //TODO(Ryan Lavin): Fill stub for Groups output
+        public string GetOutput()
+        {
+            return "";
+        }
+
+        //private member variables for each group
+        private string side;
+        private int unitCount;
+        private string tag;
+        private List<Unit> units;
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupMakerNoGui
+{
+    class Unit
+    {
+        ///////////////
+        //constructors
+        //////////////
+ 
+        //default constructor
+        public Unit()
+        {
+            id = 0;
+            side = "WEST";
+            vehicleName = "B_Soldier_TL_F";
+            UnitName = "A1 FTL";
+            rank = 0;
+            skill = 0;
+            leader = false;
+            tagedUnit = false;
+            tag = "";
+        }
+        public Unit(string side)
+        {
+            id = 0;
+            this.side = side;
+            vehicleName = "B_Soldier_TL_F";
+            UnitName = "A1 FTL";
+            rank = 0;
+            skill = 0;
+            leader = false;
+            tagedUnit = false;
+            tag = "";
+        }
+
+
+        ///////////////////
+        //member functions
+        //////////////////
+
+        //TODO(Ryan Lavin): fill stub for units GetOutput
+        public string GetOutput()
+        {
+            return "";
+        }
+
+        //////////////////////
+        //getters and setters
+        /////////////////////
+
+        //getter and setter for id
+        public int ID 
+        {
+            get 
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        //getter and setter for side
+        public string Side
+        {
+            get
+            {
+                return side;
+            }
+
+            set
+            {
+                side = value;
+            }
+        }
+
+        //getter and setter for vehicleName
+        public string VehicleName
+        {
+            get
+            {
+                return vehicleName;
+            }
+
+            set
+            {
+                vehicleName = value;
+            }
+        }
+
+        //getter and setter for unitName
+        public string UnitName
+        {
+            get
+            {
+                return unitName;
+            }
+            set
+            {
+                unitName = value;
+            }
+        }
+
+        //getter and setter for skill
+        public double Skill
+        {
+            get
+            {
+                return skill;
+            }
+
+            set
+            {
+                skill = value;
+            }
+        }
+
+        //getter and setter for the rank of the unit
+        public int Rank
+        {
+            get
+            {
+                return rank;
+            }
+
+            set
+            {
+                rank = value;
+            }
+        }
+
+        //getter and setter for leader
+        public bool Leader
+        {
+            get
+            {
+                return leader;
+            }
+            set
+            {
+                leader = value;
+            }
+        }
+
+        //getter and setter for tagedUnit
+        public bool TagedUnit
+        {
+            get
+            {
+                return tagedUnit;
+            }
+
+            set
+            {
+                tagedUnit = value;
+            }
+        }
+
+        //getter and setter for tag
+        public string Tag
+        {
+            get
+            {
+                return tag;
+            }
+            set
+            {
+                tag = value;
+            }
+        }
+
+
+        ///////////////////
+        //member variables
+        //////////////////
+
+        //position of unit in group
+        int id;
+
+        //faction the unit belongs to
+        string side;
+
+        //in engine unit name
+        string vehicleName;
+
+        //makes unit playable
+        const string player = "player=PLAY CDG";
+
+        //name of the unit seen when sloting
+        string unitName;
+
+        //skill of the unit
+        double skill;
+
+        //rank of the unit
+        int rank;
+        
+        //determines if the unit is the leader of its group
+        bool leader;
+
+        //determines if the unit gets a map marker
+        bool tagedUnit;
+
+        //string of the units map marker
+        string tag;
+
+
+    }
+}
