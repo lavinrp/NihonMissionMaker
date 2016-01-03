0412a2991c19d62e0fd280974a2b196ea82c9288
Ryan Lavin <lavinrp@mail.uc.edu>
2016-01-02 22:23:08 -0500

    Units can now import vehicle and display names. Rank combobox items filled.

--- Unit.xaml.cs
+++ Unit.xaml.cs
@@ -29,26 +29,130 @@ namespace Nihon_Mission_Maker
         public Unit(string unitString)
         {
             InitializeComponent();
-            ImportDisplayName(ref unitString);
+
+            //DisplayName
+            unitDisplayNameTextBox.Text = ImportDisplayName(ref unitString);
+
+            //side
+            side = ImportSide(ref unitString);
+            UpdateGuiSide(side);
+
+            //vehicleName
+            vehicleNameTextBox.Text = ImportVehicleName(ref unitString);
+
+            //Rank
+            
+        }
+
+        #region Import from SQM
+        /// <summary>
+        /// Imports the unit description from the mission.sqm
+        /// </summary>
+        /// <param name="unitString">Formated string from mission.sqm containing the units properties</param>
+        /// <returns></returns>
+        private string ImportDisplayName(ref string unitString)
+        {
+            //find display name
+            const string displayNameRegexExpression = "(?<=description=\")(.*)(?=\")";
+            var match = Regex.Match(unitString, displayNameRegexExpression, RegexOptions.Singleline);
+            string displayName = match.ToString();
+
+            //Return name or error
+            if (string.IsNullOrEmpty(displayName))
+            {
+                MessageBox.Show("Error reading display name of unit", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return "";
+            }
+            return displayName;
         }
 
         /// <summary>
-        /// Takes the unit description from the mission.sqm and 
+        /// Imports the unit side from the mission.sqm
         /// </summary>
-        /// <param name="unitString"></param>
-        public void ImportDisplayName(ref string unitString)
+        /// <param name="unitString">Formated string from mission.sqm containing the units properties</param>
+        /// <returns>Side of the unit</returns>
+        private Sides ImportSide(ref string unitString)
         {
-            try
+            //Find side
+            const string sideRegexExpression = "(?<=side=\")(.*?)(?=\")";
+            var match = Regex.Match(unitString, sideRegexExpression, RegexOptions.Singleline);
+            string sideString = match.ToString();
+
+            switch (sideString)
             {
-                //Expression for finding the display name
-                const string displayNameRegexExpression = "(?<=description=\")(.*)(?=\")";
-                var matches = Regex.Matches(unitString, displayNameRegexExpression, RegexOptions.Singleline);
-                unitDisplayNameTextBox.Text = matches[0].ToString();
+                case "WEST":
+                    return Sides.BLUF;
+                case "EAST":
+                    return Sides.OPF;
+                case "GUER":
+                    return Sides.IND;
+                case "CIV":
+                    return Sides.CIV;
+                case "LOGIC":
+                    return Sides.LOGIC;
+                default:
+                    MessageBox.Show("Could not import unit side", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    return Sides.EMPTY;
             }
-            catch
+
+        }
+
+        /// <summary>
+        /// Imports the vehicle parameter from the mission.sqm
+        /// </summary>
+        /// <param name="unitString">Formated string from mission.sqm containing the units properties.</param>
+        /// <returns>vehicle name found in the passed text.</returns>
+        private string ImportVehicleName(ref string unitString)
+        {
+            //find vehicle name
+            const string vehicleNameRegexExpression = "(?<=vehicle=\")(.*?)(?=\")";
+            var match = Regex.Match(unitString, vehicleNameRegexExpression, RegexOptions.Singleline);
+            string vehicleName = match.ToString();
+
+            //Return vehicle name or error
+            if (string.IsNullOrEmpty(vehicleName))
             {
-                MessageBox.Show("Error reading display name of unit", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                MessageBox.Show("Error finding unit vehicle name.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return "";
+            }
+            return vehicleName;
+        }
+
+        private void ImportRank(ref string unitString)
+        {
+            //TODO: fill stub
+        }
+        #endregion
+
+        /// <summary>
+        /// Sets the selected side in the GUI to the passed side
+        /// </summary>
+        /// <param name="side">New side to display</param>
+        private void UpdateGuiSide(Sides side)
+        {
+            switch (side)
+            {
+                case Sides.BLUF:
+                    sideComboBox.SelectedItem = sideBluefor;
+                    break;
+                case Sides.IND:
+                    sideComboBox.SelectedItem = sideIndfor;
+                    break;
+                case Sides.OPF:
+                    sideComboBox.SelectedItem = sideOpfor;
+                    break;
+                case Sides.CIV:
+                    sideComboBox.SelectedItem = sideCiv;
+                    break;
+                default:
+                    //if no valid selection select nothing
+                    sideComboBox.SelectedIndex = -1;
+                    break;
             }
         }
+
+        //Member variables
+        public Sides side;
+        public bool player;
     }
 }
